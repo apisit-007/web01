394b8cc3b841b4ee9e46934dface79749f250b4c
apisit-007 <topza5512@gmail.com>
2026-06-29 12:03:42 +0700

    Add files via upload

---
+++ workshop3/script.js
@@ -0,0 +1,10 @@
+for(i=1; i<=5; i++){
+    document.write("<h",i,"> มหาวิทยาลัยราชภัฏนครปฐม </h",i,">");
+}
+
+for(i=0; i<=10; i++){
+    for(j=0; j<=i; j++){
+        document.write("<img src='image/Gigachad.webp' width=50>");
+    }
+    document.write("<br>");
+}
